20e0a59ea75e0414d6a719b0aedc9ff56d5f1fca
lsewcx <lse210085@163.com>
2023-08-17 16:46:31 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    printf("我是printf: Hello World\n");
+    cout << "我是C++的cout "
+         << "你好，世界" << endl;
+
+    return 0;
+}
